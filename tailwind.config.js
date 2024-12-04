47eaaab3b2d6a6cbb06cde025d2ff0c7a1098ba6
mohitbohara.img <mohitbohara.img@gmail.com>
2024-12-04 12:39:07 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary:"#583FD4",
+        secondary:"#432D85"
+      },
+      fontFamily: {
+        Mulish: ["Mulish"],
+      },
+    },
+  },
+  plugins: [],
+}
+
